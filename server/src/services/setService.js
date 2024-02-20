b8c6ac3a0020c4dd649e5907f697ed7b3de81fae
Oleksandr Bezverkhnii <bezverkhniispace@gmail.com>
2024-02-20 09:02:12 +0100

    working on creating set by link endpoint

--- server/src/services/setService.js
+++ server/src/services/setService.js
@@ -1,8 +1,7 @@
 import ApiError from "../exceptions/ApiError.js"
 import Set from "../models/Set.js"
 import { v4 as uuidv4 } from "uuid"
-import { spawn } from "child_process"
-import path from "path"
+import fetch from "node-fetch"
 
 export const serviceCreateSet = async (userId, title, cards) => {
   const link = uuidv4()
@@ -51,29 +50,23 @@ export const serviceDeleteSet = async (userId, setId, setUser) => {
 export const serviceCreateSetByLink = async (userId, title) => {
   // To send ID request to Python endpoint 
   // TODO: In order not to generate twice, it makes sense to transfer this string to serviceCreateSet ?
-  // const reqId = uuidv4()
+  const reqId = uuidv4()
+  
+  // await serviceCreateSet(userId, title, data)
 
-  const pyDirname = path.join(process.cwd() + "/src/python/test.py")
-  // Execute Python script
-  const pyProcess = spawn("python", [pyDirname])
-
-  // Set encoding for stdout
-  pyProcess.stdout.setEncoding('utf8')
-
-  // Listen for data from Python script
-  pyProcess.stdout.on("data", async (data) => {
-    console.log(data)
-
-    // Call serviceCreateSet to create set
-    return await serviceCreateSet(userId, title, data)
+  const pyResponse = await fetch("", {
+    method: "post",
+    headers: {
+      "Accept": "application/json",
+      "Content-Type": "application/json"
+    },
+    body: JSON.stringify({
+      reqId,
+      resource,
+      partOfSpeech
+    })
   })
-
-  // On error
-  pyProcess.stderr.on("data", (data) => {
-    console.error(`stderr: ${data}`)
-  })
-
-  pyProcess.on("close", (code) => {
-    console.log(`child process exited with code ${code}`)
-  })
+  
+  const cards = await pyResponse.json()
+  console.log(cards)
 }
