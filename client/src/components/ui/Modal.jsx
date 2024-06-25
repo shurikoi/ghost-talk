b382c5e82bf05a88dca972024e221ce974eef3cd
Oleksandr Bezverkhnii <bezverkhniispace@gmail.com>
2024-06-25 11:01:10 +0200

    modallayout component created

--- client/src/components/ui/Modal.jsx
+++ client/src/components/ui/Modal.jsx
@@ -1,24 +1,8 @@
-import { useContext } from 'react'
-import { FormModalContext } from '../../contexts/FormModalContext'
-import styles from './ModalMenu.module.css'
 import { observer } from 'mobx-react-lite'
+import ModalLayout from './ModalLayout'
 
 function Modal({ children }) {
-  const { modalMenuStore } = useContext(FormModalContext)
-
-  return (
-    <div
-      className={`${
-        modalMenuStore.isFormActive ? 'flex' : 'hidden'
-      } h-full w-full right-0 left-0 fixed justify-center items-center`}
-    >
-      <div
-        className="absolute h-full w-full bg-black/2"
-        onClick={() => modalMenuStore.removeClass()}
-      ></div>
-      <div className="bg-white rounded-[32px] relative w-[27rem] overflow-hidden">{children}</div>
-    </div>
-  )
+  return <ModalLayout>{children}</ModalLayout>
 }
 
 export default observer(Modal)
--- client/src/components/ui/ModalLayout.jsx
+++ client/src/components/ui/ModalLayout.jsx
@@ -1,26 +1,24 @@
-export default function ModalLayout({ children }) {
-  //   return (
-    //   <div
-    //     className={`${styles.main} ${
-    //       modalMenuStore.isFormActive && styles.active
-    //     }`}
-    //   >
-  //       <div
-  //         className={styles.behind}
-  //         onClick={() => modalMenuStore.removeClass()}
-  //       ></div>
-  //       <div className={styles.form}>{children}</div>
-  //     </div>
-  //   )
+import { observer } from 'mobx-react-lite'
+import { useContext } from 'react'
+import { FormModalContext } from '../../contexts/FormModalContext'
 
+function ModalLayout({ children }) {
+  const { modalMenuStore } = useContext(FormModalContext)
   return (
     <div
-      className="hidden h-full w-full right-0 left-0 fixed justify-center items-center">
+      className={`${
+        modalMenuStore.isFormActive ? 'md:flex' : 'hidden'
+      } h-full w-full right-0 left-0 fixed justify-center items-center`}
+    >
       <div
-        className={styles.behind}
+        className="absolute h-full w-full bg-black/2"
         onClick={() => modalMenuStore.removeClass()}
       ></div>
-      <div className={styles.form}>{children}</div>
+      <div className="bg-white rounded-t-[32px] absolute md:relative bottom-0 left-0 w-full md:w-[27rem] md:rounded-[32px] overflow-hidden">
+        {children}
+      </div>
     </div>
   )
 }
+
+export default observer(ModalLayout)
